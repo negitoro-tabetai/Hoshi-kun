b475b4013ce76952c6f7968992ad823321bf37e3
hoge039 <umatama039@gmail.com>
2019-02-26 11:57:11 +0900

    敵の更新 physicsなどを２Dに変換

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Rigidbody2D _rigidbody;
 
-    Rigidbody _rigidbody;
-    [SerializeField, Tooltip("移動してからの経過時間")] float _time = 0;
+    [SerializeField, Tooltip("移動後の経過時間")] float _time = 0;
     [SerializeField, Tooltip("移動する制限時間")] float _moveTime;
-    [SerializeField, Tooltip("歩く速さ")] float _speed;
+    [SerializeField, Tooltip("移動の速さ")] float _speed;
     [SerializeField, Tooltip("ライフポイント")] int _life;
     [SerializeField, Tooltip("地面のレイヤーマスク")] LayerMask _groundLayer;
     [SerializeField, Tooltip("攻撃力")] int _attackPoint;
     [SerializeField, Tooltip("rayの長さ")] float _rayLength;
+    [SerializeField, Tooltip("プレイヤーと接触した後の、通常に戻るための距離")] float _distanceLimit = 5;
+    [SerializeField] GameObject _player;
+    //プレイヤーとの距離
+    Vector2 _playerToDistance;
+    
+    //回転する角度
     const int _rotationAngle = 180;
+    //プレイヤーと接触したか
+    bool _isTouching;
+
 
 
     public int AttackPoint
@@ -23,28 +32,52 @@ public class Enemy : MonoBehaviour
             return _attackPoint;
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_isTouching)
+        {
+            MoveTimer();
+        }
+        else
+        {
+            Direction();
+            _playerToDistance = _player.transform.position - transform.position;
+
+            //接触してから一定距離離れた場合通常に戻る
+            if(Mathf.Abs(_playerToDistance.x) >= _distanceLimit)
+            {
+                _isTouching = false;
+            }
+        }
         Damage();
-        MoveTimer();
-        Debug.DrawRay(transform.position + transform.right, Vector2.down, Color.red, Time.deltaTime);
+        //Debug.DrawRay(transform.position + transform.forward, Vector2.down, Color.red, Time.deltaTime);
     }
 
     /// <summary>
     /// 移動処理
     /// </summary>
     void FixedUpdate()
-    {
-        _rigidbody.velocity = new Vector2(transform.right.x * _speed , _rigidbody.velocity.y);
+    { 
+        if (_isTouching || 
+            !Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer))
+        {
+            
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+        else
+        {
+            _rigidbody.velocity = transform.forward * _speed;
+        }
     }
-
+    
     /// <summary>
     /// 方向転換するまでの時間計測
     /// </summary>
@@ -52,12 +85,13 @@ public class Enemy : MonoBehaviour
     {
         _time += Time.deltaTime;
         if(_time > _moveTime || 
-            !Physics.Raycast(transform.position + transform.right, Vector2.down, _rayLength, _groundLayer))
+            !Physics2D.Raycast(transform.position + transform.forward, Vector2.down, _rayLength, _groundLayer))
         {
-            Dir();
+            Direction();
             _time = 0;
         }
     }
+
     /// <summary>
     /// ダメージ処理
     /// </summary>
@@ -76,13 +110,29 @@ public class Enemy : MonoBehaviour
     /// <summary>
     /// オブジェクトの方向転換
     /// </summary>
-    void Dir()
+    void Direction()
     {
-        transform.Rotate(0, _rotationAngle, 0);
+        if (_isTouching)
+        {
+            //回転軸をyだけに制限し
+            //ターゲットの方向に方向転換する
+            Vector3 target = _player.transform.position;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+        else if(!_isTouching && 
+            Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer))
+        {
+            transform.Rotate(0, _rotationAngle, 0);
+        }
     }
-    
-    private void OnWillRenderObject()
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         
+        if(collision.gameObject.tag == "Player")
+        {
+            _isTouching = true;
+        }
     }
 }
