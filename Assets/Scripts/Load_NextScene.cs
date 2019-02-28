d96d2336a853e15eb6922034e7e9c7ae52916475
Haruka <slytherin_aquamarine_cat.1@ezweb.ne.jp>
2019-02-28 10:29:32 +0900

    ゴールを作成

---
+++ Assets/Scripts/Load_NextScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_NextScene : MonoBehaviour
+{
+    public SceneObject nextScene;
+
+    private void Update()
+    {
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    SceneManager.LoadScene(nextScene);
+        //}
+
+    }
+
+    void OntriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Load();
+        }
+    }
+
+    IEnumerator Load()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(nextScene);
+    }
+
+}
