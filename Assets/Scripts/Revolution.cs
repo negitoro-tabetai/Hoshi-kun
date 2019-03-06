8f50908a9867aaf1eef760c6919406aced62a0e1
Haruka <slytherin_aquamarine_cat.1@ezweb.ne.jp>
2019-03-06 15:43:23 +0900

    公転の個数を制限、アニメーションの調整

--- Assets/Scripts/Revolution.cs
+++ Assets/Scripts/Revolution.cs
@@ -4,50 +4,49 @@ using UnityEngine;
 
 public class Revolution : MonoBehaviour
 {
-    [SerializeField,Tooltip("弾道を表示するための点のプレファブ")]   GameObject dummyObjPref;
-    [SerializeField,Tooltip("点の親オブジェクト")]  GameObject dummyParent;
+    [SerializeField, Tooltip("弾道を表示するための点のプレファブ")] GameObject dummyObjPref;
+    [SerializeField, Tooltip("点の親オブジェクト")] GameObject dummyParent;
     [SerializeField, Tooltip("ほしくん")] GameObject Hoshikun_obj;
-    [SerializeField,Tooltip("初速のベクトル")]   Vector3 initalvelocity;
+    [SerializeField, Tooltip("初速のベクトル")] Vector3 initalvelocity;
     //弾道予測の点の数   
-    int dummyCount=50;
+    int dummyCount = 50;
     //弾道を表示する間隔の秒数
-    float secInterval=0.02f;
+    float secInterval = 0.02f;
     //点が移動する速度
     float offsetSpeed = 0.5f;
     //公転する速さ
     float speed = 300;
     //プレイヤーとどのくらい近ければ公転するか
-     float distance=1.5f;
+    float distance = 1.5f;
     [SerializeField, Tooltip("ブロックの親")] GameObject Block;
-     float this_distance;
-     float offset;
+    float this_distance;
+    float offset;
     float this_volume;
     float hoshi_volume;
     Rigidbody2D rigid;
-    
+
+    Animation animation;
+
     List<GameObject> dummySpheres = new List<GameObject>();//弾道予測を表示するための点のリスト
     bool enter;//弾道予測を表示してるかどうか
-    bool guruguru=false;//まわすスイッチ
-    bool fly_now=false;//とんでるかどうか
+    bool guruguru = false;//まわすスイッチ
+    bool fly_now = false;//とんでるかどうか
     bool small = true;//ちいさく
-    
-   
-    
+
     void Start()
     {
-        GetComponent<Animation>().enabled = false;
+        animation = GetComponent<Animation>();
 
         Off();
         rigid = GetComponent<Rigidbody2D>();
-       
 
     }
-    
+
     void Update()
     {
         //点の開始位置
         dummyParent.transform.position = Hoshikun_obj.transform.position;
-        
+
 
         //まわすかどうか判定！！！！！！！！！
         this_distance = transform.position.x - Hoshikun_obj.transform.position.x;
@@ -58,21 +57,23 @@ public class Revolution : MonoBehaviour
         this_volume = transform.localScale.x * transform.localScale.y * transform.localScale.z;
         //↑体積がほしくん以下か
 
-        if (this_distance <= distance&&this_volume<hoshi_volume)
-        {
-            if (Input.GetButtonDown("Revolution")||Input.GetKeyDown(KeyCode.R))
-            {
 
+
+
+        if (this_distance <= distance && this_volume < hoshi_volume && Hoshikun_obj.GetComponent<Revolution_Player>().Block <= 2)
+        {
+            if (Input.GetButtonDown("Revolution") || Input.GetKeyDown(KeyCode.R))
+            {
 
                 if (this.GetComponent<Enemy>() != null)
                 {
                     GetComponent<Enemy>().enabled = false;
                 }
-                
+
                 if (fly_now == false)
                 {
                     guruguru = true; //まわす
-                    
+
                     if (small == true)
                     {
                         float scaleX = transform.localScale.x;
@@ -92,19 +93,23 @@ public class Revolution : MonoBehaviour
                         if (GetComponent<Enemy>())
                         {
                             Destroy(GetComponent<Enemy>());
-
                         }
+
+                        //個数プラス
+                        Hoshikun_obj.GetComponent<Revolution_Player>().AddBlock();
+                        Hoshikun_obj.GetComponent<Revolution_Player>().Object[Hoshikun_obj.GetComponent<Revolution_Player>().Block] = this.gameObject;
+
 
                         rigid.isKinematic = true;
                         //ここで体積変える？？？？？
-                        
+
                         transform.localScale = new Vector3(scaleX / 2, scaleY / 2, scaleZ / 2);
                         small = false;
                     }
                 }
             }
         }
-        
+
         //まわる！！！！！
         if (guruguru == true)
         {
@@ -112,7 +117,9 @@ public class Revolution : MonoBehaviour
             //transform.RotateAround(Hoshikun.position, Hoshikun.up, speed * Time.deltaTime);//まわす
             Block.transform.position = Hoshikun_obj.transform.position;//親オブジェクトがほしくんについていく
 
-            GetComponent<Animation>().enabled = true;
+            
+
+            Hoshikun_obj.GetComponent<Revolution_Player>().Animation();
 
 
             this.GetComponent<BoxCollider2D>().enabled = false;//回ってる間は当たり判定オフ
@@ -127,47 +134,43 @@ public class Revolution : MonoBehaviour
 
                 dummySpheres[i].transform.localPosition = new Vector3(x, y, z);
                 //　↑鉛直上方投射の公式
-
             }
-
         }
 
         offset = Mathf.Repeat(Time.time * offsetSpeed, secInterval);
-        
+
         //Enterで飛ばす
         if (enter == true)//弾道予測がでてるときしかとばせない
         {
-           
-            if (Input.GetKeyDown(KeyCode.Return)||Input.GetButtonDown("Throw"))
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Throw"))
             {
-                Throw();
+                Hoshikun_obj.GetComponent<Revolution_Player>().Throw_();//★★★
             }
         }
     }
-    
-    void Throw()
-    {
 
+    public void Throw()
+    {
         rigid.isKinematic = false;//重力を考慮する
-        
-        
+
         float scaleX = transform.localScale.x;
         float scaleY = transform.localScale.y;
         float scaleZ = transform.localScale.z;
 
-        GetComponent<Animation>().enabled = false;
+        animation.Stop();
 
         DestroyChildObject(dummyParent.transform);
-       
+
         this.tag = "RevolutionBlock";
         this.GetComponent<BoxCollider2D>().enabled = true;//判定オンに
         this.GetComponent<BoxCollider2D>().isTrigger = true;
-     
+
         guruguru = false;
         fly_now = true;//とんでる
 
         transform.localScale = new Vector3(scaleX * 2, scaleY * 2, scaleZ * 2);//大きさ戻す
         transform.position = (dummyParent.transform.position);
+        Hoshikun_obj.GetComponent<Revolution_Player>().takeBlock();
         rigid.AddForce(initalvelocity, ForceMode2D.Impulse);//とばす
     }
 
@@ -177,7 +180,6 @@ public class Revolution : MonoBehaviour
         {
             GameObject.Destroy(parent_trans.GetChild(i).gameObject);
         }
-        
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -187,9 +189,14 @@ public class Revolution : MonoBehaviour
         {
             Destroy(gameObject);
             //ここで消す
-           Off();
+            Off();
         }
     }
+
+ 
+
+
+
 
     void Operate()
     {
@@ -215,19 +222,16 @@ public class Revolution : MonoBehaviour
         {
             initalvelocity.y--; On();
         }
-        
     }
 
     void On()
     {
         dummyParent.SetActive(true);enter = true;
-        
     }
     void Off()
     {
         dummyParent.SetActive(false);enter = false;
     }
-
-  
+    
     
 }
---
+++ Assets/Scripts/Revolution_Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolution_Player : MonoBehaviour
+{
+    [SerializeField]public GameObject[] Object;
+
+  
+    [SerializeField] int block;
+   
+    public int Block
+    {
+        get
+        {
+            return block;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void AddBlock()
+    {
+        block++;
+    }
+    public void takeBlock()
+    {
+        block--;
+    }
+
+    public void Throw_()
+    {
+        if (block == 1)
+        {
+            Object[1].GetComponent<Revolution>().Throw(); Debug.Log("1つめ");
+        }
+
+        else
+            if (block == 2)
+        {
+            Object[2].GetComponent<Revolution>().Throw(); Debug.Log("2つめ");
+        }
+        else
+        if (block == 3)
+        {
+            Object[3].GetComponent<Revolution>().Throw();
+            Debug.Log("３つめ");
+        }
+
+
+
+
+
+        //switch (block)
+        //{
+        //    case 1:
+        //        Object[1].GetComponent<Revolution>().Throw();
+        //        break;
+        //    case 2:
+        //        Object[2].GetComponent<Revolution>().Throw();
+        //        break;
+
+        //    case 3:
+        //        Object[3].GetComponent<Revolution>().Throw();
+        //        Debug.Log("aaaa");
+        //        break;
+        //}
+    }
+
+public void Animation()
+    {
+        if (block == 1)
+        {
+            Object[1].GetComponent<Animation>().Play("block"); Debug.Log("アニメ１");
+        }
+        else
+            if (block == 2)
+        {
+            Object[2].GetComponent<Animation>().Play("block 2"); Debug.Log("アニメ2");
+        }
+        else
+        if (block == 3)
+        {
+            Object[3].GetComponent<Animation>().Play("block 3"); Debug.Log("アニメ3");
+        }
+    }
+
+}
