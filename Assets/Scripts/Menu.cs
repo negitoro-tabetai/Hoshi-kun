b59d2f1d5a3cce1dfc5328445785bcc1d8e836cb
eleanor182 <in.eleanor182@gmail.com>
2019-03-06 16:23:42 +0900

    メニューを汎用化

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [System.Serializable]
+    struct Element
+    {
+        public Image image;
+        public UnityEvent _event;
+    }
+
+    enum InputAxis
+    {
+        Horizontal,
+        Vertical
+    }
+    [SerializeField] InputAxis _inputAxis;
+
+    [SerializeField] Element[] _elements;
+    [SerializeField] Color _normalColor;
+    [SerializeField] Color _selectedColor;
+
+    int _cursor;
+
+    public int Cursor
+    {
+        get
+        {
+            return _cursor;
+        }
+        set
+        {
+            _cursor = value;
+            _cursor = (int)Mathf.Repeat(_cursor, _elements.Length);
+        }
+    }
+
+    float _previousInput;
+    float _input;
+
+    public bool Increase
+    {
+        get
+        {
+            return _previousInput == 0 && _previousInput < _input;
+        }
+    }
+    public bool Decrease
+    {
+        get
+        {
+            return _previousInput == 0 && _previousInput > _input;
+        }
+    }
+
+    void Start()
+    {
+        Cursor = 0;
+        UpdateImage();
+    }
+
+    void Update()
+    {
+        if (_inputAxis == InputAxis.Horizontal)
+        {
+            _input = Input.GetAxisRaw(_inputAxis.ToString());
+        }
+        else
+        {
+            _input = Input.GetAxisRaw(_inputAxis.ToString()) * -1;
+        }
+        if (Decrease)
+        {
+            Cursor--;
+            UpdateImage();
+        }
+        if (Increase)
+        {
+            Cursor++;
+            UpdateImage();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _elements[Cursor]._event.Invoke();
+        }
+
+        //入力判定用
+        _previousInput = _input;
+    }
+
+    /// <summary>
+    /// イメージのカラーを更新する
+    /// </summary>
+    void UpdateImage()
+    {
+        for (int i = 0; i < _elements.Length; i++)
+        {
+            if (i == Cursor)
+            {
+                _elements[i].image.color = _selectedColor;
+            }
+            else
+            {
+                _elements[i].image.color = _normalColor;
+            }
+        }
+    }
+}
--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -2,99 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class StageSelect : MonoBehaviour
 {
-    [System.Serializable]
-    struct Stage
+    public void LoadScene(string sceneName)
     {
-        public string name;
-        public Image image;
+        SceneManager.LoadScene(sceneName);
     }
 
-    [SerializeField] Stage[] _stages;
-    [SerializeField] Color _normalColor, _selectedColor;
-
-    int _stageNumber;
-
-    public int StageNumber
-    {
-        get
-        {
-            return _stageNumber;
-        }
-        set
-        {
-            _stageNumber = value;
-            _stageNumber = (int)Mathf.Repeat(_stageNumber, _stages.Length);
-        }
-    }
-
-    float _previousInput;
-    float _input;
-
-    public bool MoveRight
-    {
-        get
-        {
-            return _previousInput == 0 && _previousInput < _input;
-        }
-    }
-    public bool MoveLeft
-    {
-        get
-        {
-            return _previousInput == 0 && _previousInput > _input;
-        }
-    }
-
-    void Start()
-    {
-        StageNumber = 0;
-        UpdateImage();
-    }
-
-    void Update()
+    public void AAAA()
     {
 
-        // 左右入力
-        _input = Input.GetAxisRaw("Horizontal");
-        if (MoveLeft)
-        {
-            StageNumber--;
-            UpdateImage();
-        }
-        if (MoveRight)
-        {
-            StageNumber++;
-            UpdateImage();
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            SceneManager.LoadScene(_stages[StageNumber].name);
-        }
-
-        //入力判定用
-        _previousInput = _input;
-    }
-
-    /// <summary>
-    /// イメージのカラーを更新する
-    /// </summary>
-    void UpdateImage()
-    {
-        for (int i = 0; i < _stages.Length; i++)
-        {
-            if (i == StageNumber)
-            {
-                _stages[i].image.color = _selectedColor;
-            }
-            else
-            {
-                _stages[i].image.color = _normalColor;
-            }
-        }
     }
 }
