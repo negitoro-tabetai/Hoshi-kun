15529eaeef2a5ce802ba5ec769e702852035a423
kakuda <s.kaku725@gmail.com>
2019-03-08 14:31:55 +0900

    ポーズ画面と制限時間作った

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+{
+    private GameObject pause;
+    private bool _On = false;
+    private int count=0;
+    [SerializeField]
+    void Start()
+    {
+        pause = GameObject.Find("Pause");
+        pause.SetActive(false);
+    }
+
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pause.SetActive(true);
+            _On = true;
+            count += 1;
+            Debug.Log(count);
+        }
+        if (count == 2)
+        {
+            count = 0;
+            _On = false;
+        }
+
+        if (_On == true)
+        {
+            Time.timeScale = 0;
+            Debug.Log("On");
+          
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Debug.Log("Off");
+            pause.SetActive(false);
+        }
+      
+     
+    }
+
+  public void A()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void B()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
