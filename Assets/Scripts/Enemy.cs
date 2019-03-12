d8cd307d983a4703d30d993c49556051370d200a
hoge039 <umatama039@gmail.com>
2019-03-12 10:58:57 +0900

    ナイトがプレイヤーを追い掛ける処理を実装しました ナイトをプレファブ化しました

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,23 +6,23 @@ public class Enemy : MonoBehaviour
 {
     //----------------------------------------------------------------------------------
     //変数宣言
+    //回転する角度
+    const int _rotationAngle = 180;
+    [SerializeField, Tooltip("地面のレイヤーマスク")] protected LayerMask _groundLayer;
+    [SerializeField, Tooltip("rayの長さ")] protected float _rayLength = 0.6f;
+    [SerializeField] protected GameObject _player;
+    //プレイヤーと接触したか
+    protected bool _isTouching;
+    protected Rigidbody2D _rigidbody;
+    MovableBlock _movable;
     [SerializeField, Tooltip("移動後の経過時間")] float _time = 0;
     [SerializeField, Tooltip("移動する制限時間")] float _moveTime;
     [SerializeField, Tooltip("移動の速さ")] float _speed;
-    [SerializeField, Tooltip("ライフポイント")] int _life;
     [SerializeField, Tooltip("プレイヤーと接触した後通常に戻るための距離")] float _distanceLimit = 5;
-    [SerializeField, Tooltip("地面のレイヤーマスク")] LayerMask _groundLayer;
+    [SerializeField, Tooltip("ライフポイント")] int _life;
     [SerializeField, Tooltip("攻撃力")] int _attackPoint;
-    [SerializeField, Tooltip("rayの長さ")] float _rayLength = 0.6f;
-    protected bool _isTouching;
-    protected Rigidbody2D _rigidbody;
-    [SerializeField] protected GameObject _player;
-    MovableBlock _movable;
     //プレイヤーとの距離
     Vector2 _playerToDistance;
-    //回転する角度
-    const int _rotationAngle = 180;
-    //プレイヤーと接触したか
     //----------------------------------------------------------------------------------
 
 
@@ -35,13 +35,6 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public bool IsTouching
-    {
-        get
-        {
-            return _isTouching;
-        }
-    }
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -49,6 +42,7 @@ public class Enemy : MonoBehaviour
         _movable = GetComponent<MovableBlock>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     protected void Update()
@@ -60,18 +54,19 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            Direction();
+            LookAtPlayer();
             _playerToDistance = _player.transform.position - transform.position;
 
             //接触してから一定距離離れた場合通常に戻る
-            if(Mathf.Abs(_playerToDistance.x) >= _distanceLimit)
+            if (Mathf.Abs(_playerToDistance.x) >= _distanceLimit)
             {
+                Debug.Log("はなれた");
                 _isTouching = false;
             }
         }
 
         //左右にrayを打って地面のレイヤーを持つオブジェクトに当たったらダメージを受ける
-        if(Physics2D.Raycast(transform.position , Vector2.left / 2, _rayLength, _groundLayer) &&
+        if (Physics2D.Raycast(transform.position, Vector2.left / 2, _rayLength, _groundLayer) &&
             Physics2D.Raycast(transform.position, Vector2.right / 2, _rayLength, _groundLayer))
         {
             Damage();
@@ -86,18 +81,23 @@ public class Enemy : MonoBehaviour
         if (!_isTouching &&
             Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer))
         {
-            
-            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+            _rigidbody.velocity = new Vector2(transform.right.x * _speed, _rigidbody.velocity.y);
+        }
+        else
+        {
+            Debug.Log("ふれている");
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
         }
     }
-    
+
+
     /// <summary>
     /// 方向転換するまでの時間計測
     /// </summary>
     void MoveTimer()
     {
         _time += Time.deltaTime;
-        if(_time > _moveTime || !Physics2D.Raycast(transform.position + transform.right, Vector2.down,_rayLength, _groundLayer))
+        if (_time > _moveTime || !Physics2D.Raycast(transform.position + transform.right, Vector2.down, _rayLength, _groundLayer))
         {
             Direction();
             _time = 0;
@@ -119,29 +119,33 @@ public class Enemy : MonoBehaviour
         }
     }
 
+
+    protected void LookAtPlayer()
+    {
+        //回転軸をyだけに制限し
+        //ターゲットの方向に方向転換する
+        Vector3 target = _player.transform.position;
+        target.y = transform.position.y;
+        transform.right = target - transform.position;
+    }
+
+
     /// <summary>
     /// オブジェクトの方向転換
     /// </summary>
-    void Direction()
+    protected void Direction()
     {
-        if (_isTouching)
-        {
-            //回転軸をyだけに制限し
-            //ターゲットの方向に方向転換する
-            Vector3 target = _player.transform.position;
-            target.y = transform.position.y;
-            transform.right = target - transform.position;
-        }
-        else if(!_isTouching && 
-            Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer))
+        //真下に打ったレイが当たっている場合、方向転換をする
+        if (Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer))
         {
             transform.Rotate(0, _rotationAngle, 0);
         }
     }
 
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             _isTouching = true;
         }
--- Assets/Scripts/Enemy_Knight.cs
+++ Assets/Scripts/Enemy_Knight.cs
@@ -6,27 +6,31 @@ using UnityEngine;
 [RequireComponent(typeof(MovableBlock))]
 public class Enemy_Knight : Enemy
 {
+    //----------------------------------------------------------
+    //変数宣言
     [SerializeField] float _findRayLength;
     [SerializeField] LayerMask _playerLayer;
     [SerializeField] float k_speed;
-
+    //----------------------------------------------------------
 
     protected override void Start()
     {
         base.Start();
     }
 
+
     new void Update()
     {
-        Debug.DrawRay(transform.position, transform.right * _findRayLength, Color.red);
-        if (!IsTouching && Physics2D.Raycast(transform.position, transform.right, _findRayLength, _playerLayer))
+        //向いている方向にプレイヤーがいた場合、且つ、地面と接している場合
+        if (Physics2D.BoxCast(transform.position, transform.localScale, 0, transform.right, _findRayLength, _playerLayer) && 
+            Physics2D.Raycast(transform.position + transform.right, Vector2.down, _rayLength, _groundLayer))
         {
-            Debug.Log("レイが当たった且つプレイヤに触れてない");
+            //_isTouching = true;
+            LookAtPlayer();
             Move();
         }
         else
         {
-            base.Move();
             base.Update();
         }
     }
@@ -35,9 +39,7 @@ public class Enemy_Knight : Enemy
     // Update is called once per frame
     new void Move()
     {
-        Vector3 target = (_player.transform.position - transform.position).normalized;
-        target.x = transform.position.x;
-        //_rigidbody.velocity = new Vector2(target.x * k_speed, _rigidbody.velocity.y);
-        transform.Translate(target * k_speed * Time.deltaTime);
+        //Vector3 target = (_player.transform.position - transform.position).normalized;
+        _rigidbody.velocity = new Vector2(transform.right.x * k_speed, _rigidbody.velocity.y);
     }
 }
