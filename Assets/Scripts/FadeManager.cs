e6c47bd73d6bdb62937a7fcc59e3be5073394b9c
eleanor182 <in.eleanor182@gmail.com>
2019-03-19 17:44:46 +0900

    フェードイン/アウト作成

---
+++ Assets/Scripts/FadeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeManager : SingletonMonoBehaviour<FadeManager>
+{
+    [SerializeField, Tooltip("フェード用のイメージ")] Image _fadeImage;
+    
+    new void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    /// <summary>
+    /// フェードイン -> シーン遷移 -> フェードアウト
+    /// </summary>
+    /// <param name="sceneName">遷移するシーン</param>
+    /// <param name="fadeTime">フェードイン/アウトにかける時間</param>
+    public void SceneFade(string sceneName, float fadeTime)
+    {
+        StartCoroutine(SceneFadeCoroutine(sceneName, fadeTime));
+    }
+
+    IEnumerator SceneFadeCoroutine(string sceneName, float fadeTime)
+    {
+        yield return FadeIn(fadeTime);
+        SceneManager.LoadScene(sceneName);
+        yield return FadeOut(fadeTime);
+    }
+    IEnumerator FadeOut(float fadeTime)
+    {
+        float t = 0;
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            Color color = _fadeImage.color;
+            color.a = 1 - t / fadeTime;
+            _fadeImage.color = color;
+            yield return null;
+        }
+    }
+    IEnumerator FadeIn(float fadeTime)
+    {
+        float t = 0;
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            Color color = _fadeImage.color;
+            color.a = t / fadeTime;
+            _fadeImage.color = color;
+            yield return null;
+        }
+    }
+}
