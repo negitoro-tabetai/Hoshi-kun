800d8938683acc37cb928bc4f9558ea7a511f17a
kakuda <s.kaku725@gmail.com>
2019-03-19 15:57:57 +0900

    ステージ作成

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Door;
+    [SerializeField] GameObject Camera;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag== "Player")
+        {
+            Player.transform.position = Door.transform.position;
+           Camera.transform.position = Door.transform.position;
+        }
+    }
+}
