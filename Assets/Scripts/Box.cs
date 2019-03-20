dac9cc35149830059203aa895583715e00c0fb58
Haruka <slytherin_aquamarine_cat.1@ezweb.ne.jp>
2019-03-20 11:06:52 +0900

    だいたい完成。　※アセット追加しました

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Box : MonoBehaviour
+{
+    [SerializeField] GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("到達");
+            GameObject.Find("Player").GetComponent<Player>().enabled=false;
+            Goal();
+        }
+    }
+
+    void Goal()
+    {
+        Text.SetActive(true);
+        Text.GetComponent<TextMeshProUGUI>().text = "Game Clear!!";
+        Debug.Log("テキスト表示");
+        Invoke("Load", 3.0f);
+    }
+
+   void Load()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -192,7 +192,7 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.tag == "Enemy" && !IsInvincible)
         {
-            Damage(other.gameObject.GetComponent<Enemy>().AttackPoint, other.transform.transform.position);
+            Damage(other.gameObject.GetComponent<BaseEnemy>().AttackPoint, other.transform.transform.position);
         }
     }
 
