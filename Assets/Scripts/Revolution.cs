88074e3bef900563c780f158b8a0418e44035449
hoge039 <umatama039@gmail.com>
2019-03-25 12:09:15 +0900

    particle をスクリプトで生成 再生後は消す処理を作成

--- Assets/Scripts/Revolution.cs
+++ Assets/Scripts/Revolution.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Revolution : MonoBehaviour
 {
+    [SerializeField] GameObject _revolutionBreakEffect;
     public bool On { get; set; }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (On)
         {
+            Instantiate(_revolutionBreakEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
