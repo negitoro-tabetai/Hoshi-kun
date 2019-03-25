7e06d7bc0302457f343c2481de4bf87f39d3f470
Haruka <slytherin_aquamarine_cat.1@ezweb.ne.jp>
2019-03-25 12:11:04 +0900

    詰まないシステム

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -6,26 +6,21 @@ using UnityEngine.SceneManagement;
 public class BlockGenerator : MonoBehaviour
 {
    
-    [SerializeField, Tooltip("ブロック")] GameObject block;
+    [SerializeField, Tooltip("ブロック")] GameObject[] block;
     [SerializeField, Tooltip("生成するブロックのプレファブ")] GameObject blockprefab;
-    bool on;
+ 
     public Swich swich;
     bool stop=true;
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(blockprefab, this.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.P))
-        // {
-        //     SceneManager.LoadScene("Stage2");
-        // }
-        on =swich.Swich_;
-        if (block == null && on == false)
+        block = GameObject.FindGameObjectsWithTag("RevolutionBlock");
+        if (block.Length == 0 && swich.Swich_ == false)
         {
             
             if (stop == true)
@@ -34,6 +29,7 @@ public class BlockGenerator : MonoBehaviour
                 Instantiate(blockprefab, this.transform.position, Quaternion.identity);
                 stop = false;
             }
+            stop = true;
         }
     }
 }
