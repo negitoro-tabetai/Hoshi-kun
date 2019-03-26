dc68ef973bf12daae6a4d4f648cfe09f7099c622
eleanor182 <in.eleanor182@gmail.com>
2019-03-26 10:52:11 +0900

    ステージ3修正

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -9,20 +9,27 @@ public class Teleport : MonoBehaviour
     [SerializeField] GameObject Camera;
     void Start()
     {
-        
+
     }
 
-   
+
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag== "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            Player.transform.position = new Vector3(Door.transform.position.x, Player.transform.position.y, 0);
-            Camera.transform.position = new Vector3(Door.transform.position.x, Camera.transform.position.y, Camera. transform.position.z);
+            StartCoroutine(Fade(0.5f));
         }
+    }
+
+    IEnumerator Fade(float fadeTime)
+    {
+        yield return FadeManager.Instance.FadeIn(fadeTime);
+        Player.transform.position = new Vector3(Door.transform.position.x, Player.transform.position.y, 0);
+        // Camera.transform.position = new Vector3(Door.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
+        yield return FadeManager.Instance.FadeOut(fadeTime);
     }
 }
