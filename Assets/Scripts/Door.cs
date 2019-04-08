ace8e128499936ce78d2710d1f4f0155ea0fcd21
eleanor182 <in.eleanor182@gmail.com>
2019-04-08 16:46:14 +0900

    ドアを物があったら途中で止まるように修正

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -19,6 +19,7 @@ public class Door : MonoBehaviour
     // 初期位置
     Vector3 _defaultPosition;
     [SerializeField, Tooltip("目標地点(相対)")] Vector3 _targetPosition;
+    [SerializeField] LayerMask _mask;
 
     // 経過時間
     float _elapsedTime;
@@ -49,17 +50,31 @@ public class Door : MonoBehaviour
         {
             isOpen = _buttons.All(button => button.IsPressed);
         }
-        
+
         if (isOpen)
         {
             ElapsedTime += Time.deltaTime * _moveSpeed;
         }
         else
         {
-            ElapsedTime -= Time.deltaTime * _moveSpeed;
+            // 間にものがあった場合
+            ElapsedTime = Mathf.Clamp(ElapsedTime - Time.deltaTime * _moveSpeed, 1 - (GetHitPosition() - (_defaultPosition + _targetPosition)).magnitude / _targetPosition.magnitude, 1);
         }
-
         // 経過時間に合わせて移動
         transform.position = Vector3.Lerp(_defaultPosition, _defaultPosition + _targetPosition, ElapsedTime);
+
+    }
+    Vector3 GetHitPosition()
+    {
+        const float MaxDistance = 100;
+        const float size = 0.9f;
+
+        RaycastHit2D hit = Physics2D.BoxCast(_defaultPosition + _targetPosition, transform.localScale * size, transform.eulerAngles.z, -_targetPosition, MaxDistance, _mask);
+        if (hit)
+        {
+            Debug.DrawRay(transform.position, hit.point - (Vector2)transform.position, Color.blue);
+            return new Vector3(transform.position.x, hit.point.y + Mathf.Sign(transform.position.y - hit.point.y) * Mathf.Abs((transform.rotation * transform.localScale).y) / 2, transform.position.z);
+        }
+        return _targetPosition;
     }
 }
