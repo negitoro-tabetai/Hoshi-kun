fc11b03e12552381babc8eed4bd5b097e9ff23e8
kakuda <s.kaku725@gmail.com>
2019-04-16 14:21:07 +0900

    星型Hp作成＆差し替え、Option修正、付与作成

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -8,7 +8,7 @@ public class Pause : MonoBehaviour
 
     private bool _On = false;
 
-
+ 
 
     [SerializeField] Slider _sliderSE;
     [SerializeField] Slider _sliderBGM;
@@ -24,7 +24,9 @@ public class Pause : MonoBehaviour
 
         option.SetActive(false);
 
-      
+        //AudioManagerと同じ値にしておく
+        _sliderSE.value = AudioManager.getVolumeSE();
+        _sliderBGM.value = AudioManager.getVolumeBGM();
     }
 
 
@@ -72,6 +74,7 @@ public class Pause : MonoBehaviour
         option.SetActive(false);
         pause.SetActive(true);
     }
+    //SliderでSE変更
     public void ChangeVolumeSE()
     {
 
@@ -88,6 +91,7 @@ public class Pause : MonoBehaviour
         }
 
     }
+    //SliderでBGM変更
     public void ChangeVolumeBGM()
     {
 
