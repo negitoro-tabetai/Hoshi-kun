4cd622d94644f93333173111d4d17afaeac32ae0
hoge039 <umatama039@gmail.com>
2019-04-23 14:22:10 +0900

    ビョン君とオーサマの基本動作を実装しました

---
+++ Assets/Scripts/Enemy_Byon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Byon : Enemy
+{
+    //--------------------------------------
+    //変数宣言
+    [SerializeField, Tooltip("ジャンプする高さ")] float _jumpPower;
+    //--------------------------------------
+
+
+    protected override void Move()
+    {
+        _time += Time.deltaTime;
+        if (IsGround())
+        {
+            // 時間を計測し、進む方向を変える関数を呼び出す
+            if (_time > _moveTime || !CanMoveForward())
+            {
+                Direction();
+                _time = 0;
+            }
+            _rigidbody.velocity = new Vector2(transform.right.x * _speed, _jumpPower);
+        }
+    }
+}
